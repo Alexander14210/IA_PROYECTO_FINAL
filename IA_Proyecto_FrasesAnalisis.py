6ae196bc1c584f7a00601cd98024667246df70e5
Alexander14210 <75588220+Alexander14210@users.noreply.github.com>
2022-07-13 17:14:24 -0500

    Permitir al chatbot mostrar una imagen relacionada con la frase por medio de la API de imgur

--- IA_Proyecto_FrasesAnalisis.py
+++ IA_Proyecto_FrasesAnalisis.py
@@ -8,31 +8,74 @@ Created on Wed Jun 29 22:40:16 2022
 import pandas as pd
 from textblob import TextBlob
 
+def encontrar_info_tlor(frase: str):
+    df_tlor = pd.read_csv('df_TLOR.csv', encoding='cp1252')
+    filtro_dialog = df_tlor['dialog'] == frase
+    char_pd_series = df_tlor[filtro_dialog]['char']
+    char_name_list = list(char_pd_series)
+    char_name = char_name_list[0]
+
+    return char_name.capitalize()
+
+def encontrar_info_hp(frase: str):
+    df_hp_dialog = pd.read_csv('Dialogue.csv')
+    df_hp_char = pd.read_csv('Characters.csv')
+
+    #Filtrar registro al que pertenece la frase
+    filtro_dialog = df_hp_dialog['Dialogue'] == frase
+
+    char_id = df_hp_dialog[filtro_dialog]['Character ID'].sum()
+    char_name = df_hp_char['Character Name'][char_id - 1]
+
+    return char_name.capitalize()
+
 
 
 def encontrar_polaridad(frase_inicial: str):
     df_tlor = pd.read_csv('df_TLOR.csv', encoding='cp1252')
     df_hp = pd.read_csv('Dialogue.csv')
     
+    output = {}
+
     frase = TextBlob(frase_inicial)
+
+    frase_es__de_tlor = (df_tlor['dialog'] == frase_inicial).any()
+    frase_es_de_hp = (df_hp['Dialogue'] == frase_inicial).any()
 
     frase_esta_en_df = (df_tlor['dialog'] == frase_inicial).any() or (df_hp['Dialogue'] == frase_inicial).any()
     sentimiento = ''
 
+    if frase_es__de_tlor:
+        nombre_personaje = encontrar_info_tlor(frase_inicial)
+        output['serie'] =  "tlor"
+        output['nombre'] = nombre_personaje
+    elif frase_es_de_hp:
+        nombre_personaje = encontrar_info_hp(frase_inicial)
+        output['serie'] = "hp"
+        output['nombre'] = nombre_personaje
+
+    else:
+        output['serie'] = ''
+        output['nombre'] = ''
+
+
 
     if frase.polarity >= -1.00 and frase.polarity <= -0.01:
-        sentimiento = 'negativo'
+        sentimiento = 'negativo 😕'
+        output['sentimiento'] = sentimiento
         print("La frase ingresada presenta un sentimiento negativo")
 
     elif frase.polarity >= 0.00 and frase.polarity <= 0.99:
-        sentimiento = 'neutral'
+        sentimiento = 'neutral 😐'
+        output['sentimiento'] = sentimiento
         print("La frase ingresada presenta un sentimiento neutral")
 
     elif frase.polarity >= 1.00:
-        sentimiento = 'positivo'
+        sentimiento = 'positivo 😄'
+        output['sentimiento'] = sentimiento
         print("La frase ingresada presenta un sentimiento positivo")
 
-    return sentimiento, frase_esta_en_df
+    return output
 
 
 
--- botResponder.py
+++ botResponder.py
@@ -1,47 +1,80 @@
 # bot.py
+
 import os
+from re import I
 
 from discord.ext import commands
 import discord
 import aiohttp
+import urllib
 from dotenv import load_dotenv
+import random
 from IA_Proyecto_FrasesAnalisis import encontrar_polaridad
+
 
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+IMGUR_ID = os.getenv('IMGUR_CLIENT_ID')
 
 bot = commands.Bot(command_prefix='!')
+
+
+headers = {
+  "accept": "application/json",
+  'Authorization': "Client-ID " + IMGUR_ID
+  }
 
 #Ejemplo: !emocion "Don't wanna wake up, coz I'm happier here"
 @bot.command(name='emocion', help='Ingresa una frase para descubrir la emocion asociada')
 async def emocion(ctx, frase: str):
   
-  emocionAsociada, frase_esta_en_df = encontrar_polaridad(frase)
-  output = 'Pienso que la frase que introduciste presenta un sentimiento ' + emocionAsociada
-
-  if frase_esta_en_df:
-    output += '\nEncontre esta frase en el set de datos'
+  resultado = encontrar_polaridad(frase)
+  title = 'Pienso que la frase que introduciste presenta un sentimiento ' + resultado['sentimiento']
 
 
+  
+  if resultado['serie'] == 'tlor':
+    output = '¡Hey! Mira que coincidencia, ¡está frase es de El Señor de los Anillos! \nLa dijo ' + resultado['nombre']
+    personaje_a_buscar = resultado['nombre']
+    saga_a_buscar = 'The Lord of The Rings'
 
+  elif resultado['serie'] == 'hp':
+    output = '¡Hey! Mira que coincidencia, ¡está frase es de Harry Potter! \nLa dijo ' + resultado['nombre']
+    personaje_a_buscar = resultado['nombre']
+    saga_a_buscar = 'Harry Potter'
+
+  else:
+    if resultado['sentimiento'] == 'positivo 😄':
+      output = "¡Me gustó esa frase! \nEspero que tengas un buen día."
+      personaje_a_buscar = 'dog'
+      saga_a_buscar = 'happy'
+    elif resultado['sentimiento'] == 'negativo 😕':
+      output = "Espero que tú estado de ánimo pueda mejorar, ¡Vendrán días mejores!"
+      personaje_a_buscar = 'cat'
+      saga_a_buscar = 'neutral'
+    elif resultado['sentimiento'] == 'neutral 😐':
+      output = "No todos los días tienen que ser una aventura, a veces, hay felicidad en la cotidianidad"
+      personaje_a_buscar = 'happy cat'
+      saga_a_buscar = 'happy'
+
+  input_url = f"https://api.imgur.com/3/gallery/search/top/all/?q_all={personaje_a_buscar}&q_any={saga_a_buscar}&q_type=jpg"
+
+ 
   async with aiohttp.ClientSession() as cs:
-      async with cs.get("https://random.dog/woof.json") as r:  
+      async with cs.request("GET", input_url, headers=headers) as r:  
           data = await r.json()
-          url = data['url']
+          list_data = data['data']
+          image_url = list_data[0]['images'][0]['link']
 
-          embed = discord.Embed( 
-                        title="OUTPUT", 
-                        description=output, 
-                        colour=discord.Colour.purple() 
-                        )
+          
+          embed = discord.Embed(title=title, description=output, colour=discord.Colour.purple())
+                        
+          embed.set_image(url=image_url)
 
-          embed.set_image(url=url)
-
-          await ctx.send(output, embed=embed)
-
-
+          await ctx.send(embed=embed)
+          
 
 
 @bot.event
